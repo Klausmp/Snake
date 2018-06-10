34ce8be44eb8632615d90a2b3e0c9a24d95577cf
Klausmp <cartvdennis@gmail.com>
2018-06-10 16:08:28 +0200

    add Options in the Menue (Field Wight, height, speed)

--- src/world/WorldOne.java
+++ src/world/WorldOne.java
@@ -5,12 +5,20 @@ import entity.Player;
 import entity.Snake;
 
 public class WorldOne extends World {
+    private int rotation;
+
     public WorldOne(int worldSizeX, int worldSizeY, int WORLDSPEED) {
         super(worldSizeX, worldSizeY, WORLDSPEED);
-        snakeList.add(new Player(5, 5, 3));
+        if (worldSizeX > worldSizeY) {
+            rotation = 2;
+        }
+        else {
+            rotation = 1;
+        }
+        snakeList.add(new Player(worldSizeX / 2, worldSizeY / 2, rotation));
         snakeList.add(new Snake(-320, -320, 1));
         snakeList.add(new Snake(-320, -320, 2));
         snakeList.add(new Snake(-320, -320, 3));
-        foodList.add(new Food(3,3));
+        foodList.add(new Food(3, 3));
     }
 }
